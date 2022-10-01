361d0a3a9e01cc0bc95921b142d0fbfda17da544
RomanAhoshton <109685884+RomanAhoshton@users.noreply.github.com>
2022-10-01 20:46:55 +0300

    123

---
+++ script.js
@@ -0,0 +1,20 @@
+const burger_menu=document.querySelector(".burger_menu");
+const header_list=document.querySelector(".header_list");
+
+
+
+
+burger_menu.addEventListener("click", () =>{
+    burger_menu.classList.toggle("active");
+    header_list.classList.toggle("active");
+    
+   
+  
+   
+    
+
+
+});
+
+
+
