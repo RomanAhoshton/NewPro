e9a64d8a820f83d1fe92be9ffe3aee7270172744
RomanAhoshton <109685884+RomanAhoshton@users.noreply.github.com>
2022-10-17 21:15:13 +0300

    Fix header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const burger_menu=document.querySelector(".burger_menu");
 const header_list=document.querySelector(".header_list");
+const body_lock=document.querySelector(".body_lock");
 
 
 
@@ -7,6 +8,7 @@ const header_list=document.querySelector(".header_list");
 burger_menu.addEventListener("click", () =>{
     burger_menu.classList.toggle("active");
     header_list.classList.toggle("active");
+    body_lock.classList.toggle("lock");
     
    
   
